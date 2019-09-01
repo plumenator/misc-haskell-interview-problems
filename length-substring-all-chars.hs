07d38afad6330f2c0040e5b24fbdf9503f24b753
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2019-09-01 21:05:08 +0800

    Start problem: length of smallest substring with all chars

---
+++ length-substring-all-chars.hs
@@ -0,0 +1,8 @@
+-- Calculate the length of the smallest substring containing all of
+-- the distinct characters of a string.
+
+-- Examples:
+
+-- solve "aabbc" == 4
+-- solve "geeksforgeeks" == 7
+-- solve "abababcdefababcdab" == 6
